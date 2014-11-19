5ddf06f30afbd843d1fa70bd5e552319c91cbc2a
Nath <nath@nath-VirtualBox.(none)>
2014-11-19 21:06:53 +0100

    Casi finalizado con error y pep8 pasado

--- server.py
+++ server.py
@@ -14,7 +14,7 @@ import os
 if len(sys.argv) != 4:
     print "Usage: python server.py IP port audio_file"
     raise SystemExit
-    
+
 # IP del servidor y FICHERO del audio
 IP = sys.argv[1]
 FICHERO = sys.argv[3]
@@ -27,13 +27,13 @@ except ValueError:
     raise SystemExit
 except IndexError:
     print "Usage: python client.py method receiver@IP:SIPport"
-    raise SystemExit  
+    raise SystemExit
 
-try: 
+try:
     file = open(FICHERO, 'r+')
 except IOError:
     print "Error: Fichero de audio no encontrado"
-    raise SystemExit 
+    raise SystemExit
 
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
@@ -49,7 +49,7 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
             print "El cliente nos manda " + line
             lista = line.split()
             metodo = lista[0]
-        
+
             if metodo == 'INVITE':
                 self.wfile.write('SIP/2.0 100 TRYING\r\n\r\n')
                 print 'Enviando: ' + 'SIP/2.0 100 TRYING\r\n\r\n'
@@ -58,7 +58,7 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                 self.wfile.write('SIP/2.0 200 OK\r\n\r\n')
                 print 'Enviando: ' + 'SIP/2.0 200 OK\r\n\r\n'
             elif metodo == 'ACK':
-                # aEjecutar es un string con lo que se ha de ejecutar en la shell
+                # aEjecutar: string con lo que se ha de ejecutar en la shell
                 aEjecutar = 'mp32rtp -i 127.0.0.1 -p 23032 < ' + FICHERO
                 print "Vamos a ejecutar", aEjecutar
                 os.system(aEjecutar)
