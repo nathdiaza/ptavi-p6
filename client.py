d1c9930d95b4eeeb2de131e3cc78cc91e2799394
javier martinez molina <molina@iota14.aulas.gsyc.es>
2014-11-21 17:27:17 +0100

    Tratamiento de mensajes con metodo INVITE(comprobacion de trying, ringing y OK para envío de ACK) y BYE peer-to-peer

--- client.py
+++ client.py
@@ -12,22 +12,30 @@ NAME_PROGRAM = sys.argv[0]
 METODO = sys.argv[1]
 LOGIN = sys.argv[2].split("@")[0]
 R_IP_PORT = sys.argv[2].split("@")[1]
-IP = R_IP_PORT.split(":")[0]
-PORT = R_IP_PORT.split(":")[1]
+IP_SERVER = R_IP_PORT.split(":")[0]
+PORT = int(R_IP_PORT.split(":")[1])
 
 # Contenido que vamos a enviar
-LINE = "sip:" + METODO + "@" + IP + " SIP/2.0\r\n\r\n"
+LINE = METODO + " sip:" + LOGIN + "@" + IP_SERVER + " SIP/2.0\r\n\r\n"
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+my_socket.connect((IP_SERVER, PORT))
 
 print "Enviando: " + LINE
 my_socket.send(LINE + '\r\n')
 data = my_socket.recv(1024)
 
-print 'Recibido -- ', data
+print 'Recibido -- \r\n\r\n', data
+
+if METODO == "INVITE":
+    if data.split("\r\n\r\n")[0] == "SIP/2.0 100 Trying":
+        if data.split("\r\n\r\n")[1] == "SIP/2.0 180 Ringing":
+            if data.split("\r\n\r\n")[2] == "SIP/2.0 200 OK":
+                ack = "ACK sip:" + LOGIN + "@" + IP_SERVER + " SIP/2.0\r\n\r\n"
+                my_socket.send(ack + "\r\n")
+
 print "Terminando socket..."
 
 # Cerramos todo
--- server.py
+++ server.py
@@ -13,13 +13,18 @@ class SipHandler(SocketServer.DatagramRequestHandler):
     """
 
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write("Hemos recibido tu peticion")
+
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            print "El cliente nos manda " + line
-
+            metodo = line.split(" ")[0]
+            print line
+            if metodo == "INVITE":
+                self.wfile.write("SIP/2.0 100 Trying\r\n\r\n")
+                self.wfile.write("SIP/2.0 180 Ringing\r\n\r\n")
+                self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
+            elif metodo == "BYE":
+                self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
@@ -27,5 +32,5 @@ class SipHandler(SocketServer.DatagramRequestHandler):
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     serv = SocketServer.UDPServer(("", 6001), SipHandler)
-    print "Lanzando servidor UDP de eco..."
+    print "listening...\r\n"
     serv.serve_forever()
