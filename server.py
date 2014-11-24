c2ef356207f5a1be90ccd69f3dcfac10a61be318
natalia diaz ayuste <ndiaz@iota18.aulas.gsyc.es>
2014-11-24 11:01:32 +0100

    Ultimos errores corregidos

--- server.py
+++ server.py
@@ -17,7 +17,7 @@ if len(sys.argv) != 4:
 
 # IP del servidor y FICHERO del audio
 IP = sys.argv[1]
-FICHERO = sys.argv[3]
+FICH = sys.argv[3]
 
 # Errores en los valores: PUERTO y FILE
 try:
@@ -30,10 +30,12 @@ except IndexError:
     raise SystemExit
 
 try:
-    file = open(FICHERO, 'r+')
+    file = open(FICH, 'r+')
 except IOError:
     print "Error: Fichero de audio no encontrado"
     raise SystemExit
+
+metodos_SIP = ("INVITE", "BYE", "ACK")
 
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
@@ -47,32 +49,39 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
             if not line:
                 break
             print "El cliente nos manda " + line
-            lista = line.split()
-            metodo = lista[0]
+            metodo = line.split()[0]
+            direc = line.split()[1]
+            sip = line.split()[2]
+            protocolo = direc.split(':')[0]
+            login_ip = direc.split(':')[1]
 
-            if metodo == 'INVITE':
-                self.wfile.write('SIP/2.0 100 TRYING\r\n\r\n')
-                print 'Enviando: ' + 'SIP/2.0 100 TRYING\r\n\r\n'
-                self.wfile.write('SIP/2.0 180 RING\r\n\r\n')
-                print 'Enviando: ' + 'SIP/2.0 180 RING\r\n\r\n'
-                self.wfile.write('SIP/2.0 200 OK\r\n\r\n')
-                print 'Enviando: ' + 'SIP/2.0 200 OK\r\n\r\n'
-            elif metodo == 'ACK':
-                # aEjecutar: string con lo que se ha de ejecutar en la shell
-                aEjecutar = './mp32rtp -i 127.0.0.1 -p 23032 < ' + FICHERO
-                print "Vamos a ejecutar", aEjecutar
-                os.system(aEjecutar)
-                print "\r\nEl fichero de audio ha finalizado\r\n\r\n"
-            elif metodo == 'BYE':
-                self.wfile.write('SIP/2.0 200 OK\r\n\r\n')
-                print 'Enviando: ' + 'SIP/2.0 200 OK\r\n\r\n'
-            elif metodo == 'OPTIONS' or 'REGISTER' or 'CANCEL':
+            if not metodo in metodos_SIP:
                 self.wfile.write('SIP/2.0 405 Method Not Allowed\r\n\r\n')
+                print 'Enviando: SIP/2.0 405 Method Not Allowed\r\n\r\n'
             else:
-                self.wfile.write('SIP/2.0 400 Bad Request\r\n\r\n')
+                if protocolo == "sip" and "@" in login_ip and sip == "SIP/2.0":
+                    if metodo == 'INVITE':
+                        self.wfile.write('SIP/2.0 100 Trying\r\n\r\n')
+                        print 'Enviando: ' + 'SIP/2.0 100 Trying\r\n\r\n'
+                        self.wfile.write('SIP/2.0 180 Ringing\r\n\r\n')
+                        print 'Enviando: ' + 'SIP/2.0 180 Ringing\r\n\r\n'
+                        self.wfile.write('SIP/2.0 200 OK\r\n\r\n')
+                        print 'Enviando: ' + 'SIP/2.0 200 OK\r\n\r\n'
+                    elif metodo == 'ACK':
+                        # aEjecutar: lo que se ha de ejecutar en la shell
+                        aEjecutar = './mp32rtp -i 127.0.0.1 -p 23032 < ' + FICH
+                        print "Vamos a ejecutar", aEjecutar
+                        os.system(aEjecutar)
+                        print "\r\nEl fichero de audio ha finalizado\r\n\r\n"
+                    elif metodo == 'BYE':
+                        self.wfile.write('SIP/2.0 200 OK\r\n\r\n')
+                        print 'Enviando: ' + 'SIP/2.0 200 OK\r\n\r\n'
+                else:
+                    self.wfile.write('SIP/2.0 400 Bad Request\r\n\r\n')
+                    print 'Enviando: SIP/2.0 400 Bad Request\r\n\r\n'
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     serv = SocketServer.UDPServer(("", PUERTO), EchoHandler)
-    print "\nListening... \r\n"
+    print "\nListening...\r\n"
     serv.serve_forever()
