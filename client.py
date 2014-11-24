7af6e35fd712d3c898d18031655540f76628e19e
Juan Carlos Diaz Bautista <jcdb@iota02.aulas.gsyc.es>
2014-11-24 11:32:42 +0100

    client.py PEP8

--- client.py
+++ client.py
@@ -7,34 +7,35 @@ Programa cliente SIP envia INVITE, BYE Y ack
 import socket
 import sys
 
-if len(sys.argv) != 3 or "@" not in sys.argv[2]:    
+
+if len(sys.argv) != 3 or "@" not in sys.argv[2]:
     sys.exit("Usage: python cliente.py method receiver@IP:SIPport")
 
 METHOD = sys.argv[1]
-LINE = sys.argv[2]
+line = sys.argv[2]
 SIP_ADDRES = LINE.split("@")[0]
-LINE = LINE.split("@")[1]
+line = line.split("@")[1]
 
-if ":" not in LINE:
+if ":" not in line:
     sys.exit("Usage: python cliente.py method receiver@IP:SIPport")
 
-SERVER = LINE.split(":")[0]
-PORT = int(LINE.split(":")[1])
+SERVER = line.split(":")[0]
+PORT = int(line.split(":")[1])
 
 try:
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((SERVER, PORT))
-    LINE = METHOD + " sip:" + SIP_ADDRES + "@" + SERVER + " SIP/2.0"
-    print  LINE
-    my_socket.send(LINE + '\r\n\r\n')
-    data = my_socket.recv(1024)    
+    line = METHOD + " sip:" + SIP_ADDRES + "@" + SERVER + " SIP/2.0"
+    print line
+    my_socket.send(line + '\r\n\r\n')
+    data = my_socket.recv(1024)
     print 'Recibido -- \r\n', data
     data = data.split("SIP/2.0 ")
     if "200 OK\r\n\r\n" in data and METHOD == "INVITE":
-        LINE = "ACK sip:" + SIP_ADDRES + "@" + SERVER + " SIP/2.0"
-        my_socket.send(LINE + '\r\n\r\n')
-        print  LINE
+        line = "ACK sip:" + SIP_ADDRES + "@" + SERVER + " SIP/2.0"
+        my_socket.send(line + '\r\n\r\n')
+        print line
     my_socket.close()
 except:
 
