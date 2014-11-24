399f95bad533c88468d883bf5186ab6c19fb4ce5
Juan Carlos Diaz Bautista <jcdb@iota02.aulas.gsyc.es>
2014-11-24 11:33:03 +0100

    server.py PEP8

--- server.py
+++ server.py
@@ -7,11 +7,12 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 import SocketServer
 import sys
 import os
+
+
 class EchoHandler(SocketServer.DatagramRequestHandler):
     """
     SIP server class(INVITE,ACK y BYE)
     """
-
     def handle(self):
 
         while 1:
@@ -19,17 +20,18 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
             if not line:
                 break
             print line
-            method = line.split(" ")[0]
-            if method == "INVITE":
+            line = line.split(" ")
+            method = line[0]
+            if "SIP/2.0\r\n\r\n" not in line:
+                self.wfile.write("SIP/2.0 400 Bad Request\r\n\r\n")
+            elif method == "INVITE":
                 self.wfile.write("SIP/2.0 100 Trying\r\n\r\n")
-                self.wfile.write("SIP/2.0 180 Ring\r\n\r\n")
+                self.wfile.write("SIP/2.0 180 Ringing-\r\n\r\n")
                 self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
             elif method == "ACK":
-                aEjecutar = ('./mp32rtp -i ' + IP + ' -p 23032 < ' + \
-                fich_audio)
+                aEjecutar = ('./mp32rtp -i ' + IP + ' -p 23032 < ' + fich_audio)
                 os.system(aEjecutar)
                 print aEjecutar
-                
             elif method == "BYE":
                 self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
             else:
