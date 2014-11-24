980d47b143aa209a8a73843e2352b0ca4a0502f3
Juan Carlos Diaz Bautista <jcdb@iota02.aulas.gsyc.es>
2014-11-24 10:53:53 +0100

    programa server acabado

--- server.py
+++ server.py
@@ -5,27 +5,44 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import SocketServer
-
-
+import sys
+import os
 class EchoHandler(SocketServer.DatagramRequestHandler):
     """
-    Echo server class
+    SIP server class(INVITE,ACK y BYE)
     """
 
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write("Hemos recibido tu peticion")
-        while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
-            line = self.rfile.read()
-            print "El cliente nos manda " + line
 
-            # Si no hay más líneas salimos del bucle infinito
+        while 1:
+            line = self.rfile.read()
             if not line:
                 break
+            print line
+            method = line.split(" ")[0]
+            if method == "INVITE":
+                self.wfile.write("SIP/2.0 100 Trying\r\n\r\n")
+                self.wfile.write("SIP/2.0 180 Ring\r\n\r\n")
+                self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
+            elif method == "ACK":
+                aEjecutar = ('./mp32rtp -i ' + IP + ' -p 23032 < ' + \
+                fich_audio)
+                os.system(aEjecutar)
+                print aEjecutar
+                
+            elif method == "BYE":
+                self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
+            else:
+                self.wfile.write("SIP/2.0 405 Method Not Allowed\r\n\r\n")
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = SocketServer.UDPServer(("", 6001), EchoHandler)
-    print "Lanzando servidor UDP de eco..."
+    IP = sys.argv[1]
+    PORT = int(sys.argv[2])
+    fich_audio = sys.argv[3]
+    try:
+        serv = SocketServer.UDPServer((IP, PORT), EchoHandler)
+    except (IndexError, ValueError):
+        sys.exit("Usage: python servidor.py IP port audio_file")
+    print "Listening...."
     serv.serve_forever()
